87cc16c3b2cf6141db6fda74239e4aebe746b159
Shivesh <shiveshs98@gmail.com>
2019-08-18 03:34:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Zoom from "react-reveal/Zoom";
 import "./App.css";
 
 import Header from "./components/layout/Header";
+import BackgroundSlider from "react-background-slider";
 
 export class App extends Component {
   render() {
     return (
       <Fragment>
         <Header />
-
         <div className="App">
           <h1>
             <Zoom left cascade>
@@ -23,6 +23,14 @@ export class App extends Component {
             </Zoom>
           </h1>
         </div>
+        <BackgroundSlider
+          images={[
+            "https://wallpaperplay.com/walls/full/4/9/6/102687.jpg",
+            "https://wallpaperplay.com/walls/full/a/6/3/102638.jpg"
+          ]}
+          duration={3}
+          transition={1.5}
+        />
       </Fragment>
     );
   }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import leo from "../../assets/leo.png";
 
 export class Header extends Component {
   render() {
     return (
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">LEO</Navbar.Brand>
+      <Navbar className="navb" variant="dark">
+        <Navbar.Brand href="#home">
+          <img className="leo" alt="Leo" src={leo} />
+        </Navbar.Brand>
         <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Events</Nav.Link>
