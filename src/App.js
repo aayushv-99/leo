2df173e0f44570a1d198fa0396254aa9809f1965
Shivesh <shiveshs98@gmail.com>
2019-08-18 20:06:14 +0530

    Homepage updated

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React, { Component, Fragment } from "react";
 import Zoom from "react-reveal/Zoom";
 import "./App.css";
-
 import Header from "./components/layout/Header";
+import About from "./components/layout/About";
 import BackgroundSlider from "react-background-slider";
+import sc from "./assets/sc.png";
+import bv from "./assets/bv.png";
 
 export class App extends Component {
   render() {
     return (
       <Fragment>
+        <div className="back">
+          <BackgroundSlider images={[sc, bv]} duration={3} transition={1.5} />
+        </div>
         <Header />
         <div className="App">
           <h1>
             <Zoom left cascade>
-              <div style={{ display: "inline-block" }}>OMEGA </div>
+              <div className="om">OMEGA </div>
             </Zoom>
             <Zoom cascade>
-              <div style={{ display: "inline-block" }}> LEO </div>
+              <div className="le"> LEO </div>
             </Zoom>
             <Zoom right cascade>
-              <div style={{ display: "inline-block" }}> CLUB</div>
+              <div className="cl"> CLUB</div>
             </Zoom>
           </h1>
+          <h3 className="tagline1">
+            <i>Together We Serve</i>
+          </h3>
         </div>
-        <BackgroundSlider
-          images={[
-            "https://wallpaperplay.com/walls/full/4/9/6/102687.jpg",
-            "https://wallpaperplay.com/walls/full/a/6/3/102638.jpg"
-          ]}
-          duration={3}
-          transition={1.5}
-        />
+        <About />
       </Fragment>
     );
   }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import leo from "../../assets/leo.png";
+import omega from "../../assets/omega.png";
 
 export class Header extends Component {
   render() {
     return (
       <Navbar className="navb" variant="dark">
         <Navbar.Brand href="#home">
-          <img className="leo" alt="Leo" src={leo} />
+          <img className="leo" alt="Leo" src={omega} />
         </Navbar.Brand>
         <Nav className="ml-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Events</Nav.Link>
-          <Nav.Link href="#pricing">Gallery</Nav.Link>
+          <Nav.Link href="#home">
+            <h4>Home</h4>
+          </Nav.Link>
+          <Nav.Link href="#team">
+            <h4>Team</h4>
+          </Nav.Link>
+          <Nav.Link href="#events">
+            <h4>Events</h4>
+          </Nav.Link>
+          <Nav.Link href="#gallery">
+            <h4>Gallery</h4>
+          </Nav.Link>
         </Nav>
       </Navbar>
     );
